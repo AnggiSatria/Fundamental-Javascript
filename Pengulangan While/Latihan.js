902cbe657fb21a241db1db5e5cdc445b35f0519e
AnggiSatria <anggisatria122@gmail.com>
2022-08-18 14:54:17 +0700

    comment

--- Pengulangan While/Latihan.js
+++ Pengulangan While/Latihan.js
@@ -1,6 +1,11 @@
+// pendeklkarasian variable berisi nilai integer dan dimulai dari angka 1
 let angkot = 1
 
+// pengulangan while apabila variabel angkot kuraNG dari 10
 while(angkot <= 10){
+    // cetak string angkot sebanyak 10 x
     console.log("Angkot No " + angkot + " beroperasi dengan baik");
+    // varible angkot selalu melakukan increment
     angkot++
 }
+
