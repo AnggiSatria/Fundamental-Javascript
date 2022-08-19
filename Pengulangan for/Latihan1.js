90dc24c1adc3f20c91067f0766bdb65869c32f5a
AnggiSatria <anggisatria122@gmail.com>
2022-08-19 10:23:15 +0700

    updated

---
+++ Pengulangan for/Latihan1.js
@@ -0,0 +1,19 @@
+// variable
+
+let jmlAngkot = 10;
+let angkotBeroperasi = 6;
+let noAngkot = 1
+
+// while
+
+while(noAngkot <= angkotBeroperasi){
+    console.log(`Angkot ${noAngkot} Beroperasi Dengan Baik`);
+    noAngkot++
+}
+
+// for
+
+for(noAngkot = angkotBeroperasi + 1; noAngkot <= jmlAngkot; noAngkot++){
+    console.log(`Angkot ${noAngkot} Sedang Tidak beroperasi`);
+}
+
